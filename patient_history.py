166bbcc726d2a8a59c9de6815bcc4d4cfb58e53f
David Li <63334933+9t8@users.noreply.github.com>
2024-09-14 19:03:10 -0400

    add entry components
    Signed-off-by: David Li <63334933+9t8@users.noreply.github.com>

--- patient_history.py
+++ patient_history.py
@@ -4,6 +4,41 @@ import flet as ft
 from supabase import Client
 
 
+class NewEntry(ft.Row):
+    def __init__(self, cb: Callable) -> None:
+        super().__init__()
+        self.name = ft.TextField(hint_text="Entry type")
+        self.value = ft.TextField(hint_text="Description")
+        self.controls = [
+            self.name,
+            self.value,
+            ft.FloatingActionButton(
+                icon=ft.icons.ADD,
+                on_click=lambda _: cb(self.name.value, self.value.value),
+            ),
+        ]
+
+
+class SuggestedEntry(ft.Row):
+    def __init__(self, cb: Callable, name: str) -> None:
+        super().__init__()
+        self.value = ft.TextField(hint_text="Description")
+        self.controls = [
+            ft.Text(name),
+            self.value,
+            ft.FloatingActionButton(
+                icon=ft.icons.ADD,
+                on_click=lambda _: cb(name, self.value.value),
+            ),
+        ]
+
+
+class Entry(ft.Row):
+    def __init__(self, name: str, value: str) -> None:  # add date/time
+        super().__init__()
+        self.controls = [ft.Text(name), ft.Text(value)]
+
+
 class PatientHistory:
     def __init__(
         self,
@@ -20,13 +55,9 @@ class PatientHistory:
         self.controls = ft.Column(
             [
                 ft.TextButton("Exit", on_click=lambda _: self.on_exit()),
-                ft.Row(
-                    [
-                        ft.TextField(hint_text="Entry type"),
-                        ft.TextField(hint_text="Description"),
-                        ft.FloatingActionButton(icon=ft.icons.ADD),
-                    ],
-                ),
+                NewEntry(print),
+                SuggestedEntry(print, "Suggestion"),
+                Entry("Name", "Value"),
             ],
         )
 
